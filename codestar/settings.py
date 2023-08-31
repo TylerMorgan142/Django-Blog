7c7d0c64f94cee54a0d5cee3a809df403795869c
TylerMorgan142 <tylermorgan142@gmail.com>
2023-08-31 18:48:15 +0000

    Added account authorisation

--- codestar/settings.py
+++ codestar/settings.py
@@ -33,7 +33,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['8000-tylermorgan1-djangoblog-m6l6uhfsjoh.ws-eu104.gitpod.io', 'tyler-django-blog.herokuapp.com']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -42,12 +41,22 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'django_summernote',
     'blog',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -137,7 +146,7 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
