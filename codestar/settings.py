0e6365f4d7674bc2baa6b781e9db9ad4c36401cf
TylerMorgan142 <tylermorgan142@gmail.com>
2023-09-01 22:23:38 +0000

    Deployment commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['8000-tylermorgan1-djangoblog-m6l6uhfsjoh.ws-eu104.gitpod.io', 'tyler-django-blog.herokuapp.com']
 
